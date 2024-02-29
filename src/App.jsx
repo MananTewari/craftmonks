6b71f56e9e3035a85025783f8f5eb666a651af3a
MananTewari <85873623+MananTewari@users.noreply.github.com>
2024-02-29 22:09:29 +0530

    added a router config to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
-    return ( <h1>Hello</h1> );
-}
+import React from "react";
+import { Nav } from "./Components/Navbar";
+import {Routes, Route} from "react-router-dom"
+import { Home, About, Cart, Login } from "./Components/Pages";
 
+function App(){
+  return(
+    <div className="App">
+    <Nav/>
+    <Routes>
+<Route path="/home" element={<Home />}></Route>
+<Route path="/about" element={<About />}></Route>
+<Route path="/departments" element={<Cart />}></Route>
+<Route path="/login" element={<Login />}></Route>
+    </Routes>
+    
+    
+    
+    
+    </div>
+  );
+}
 export default App;
