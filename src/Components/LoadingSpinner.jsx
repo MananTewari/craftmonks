377113ccc71235107197d871465c4d1c464c6137
MananTewari <85873623+MananTewari@users.noreply.github.com>
2024-03-15 09:50:58 +0530

    15/3 update

--- src/Components/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner.jsx
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function LoadingSpinner() {
   return (
     <div class="clearfix spinner">
-  <div class="spinner-border"
-  style={{width:"5rem", height:"5rem",marginLeft:"50%" }}
-  role="status">
-  </div>
-</div>
+      <div
+        class="spinner-border"
+        style={{ width: "5rem", height: "5rem", marginLeft: "50%" }}
+        role="status"
+      ></div>
+    </div>
   );
 }
 
