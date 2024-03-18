68c9b8cee8a827fe06e920e65ae0ae672f7c70dd
MananTewari <85873623+MananTewari@users.noreply.github.com>
2024-03-18 22:32:27 +0530

    updated empty cart

---
+++ src/Components/EmptyCart.jsx
@@ -0,0 +1,15 @@
+
+
+
+function EmptyCart() {
+  return (
+    <div className="emptycart">
+   <h1>Nothing Here</h1>
+   <h2>Shop Now</h2>
+   </div>
+  );
+}
+
+export default EmptyCart;
+
+//style this component
--- src/routes/ShoppingBag.jsx
+++ src/routes/ShoppingBag.jsx
@@ -1,24 +1,29 @@
 import "../index.css";
 import BagItem from "../Components/BagItems";
 import { useSelector } from "react-redux";
+import EmptyCart from "../Components/EmptyCart";
 
 function ShoppingBag() {
   console.log("ShoppingBag component rendered"); // Log when ShoppingBag component renders
   const bagItems = useSelector((state) => state.bag);
   const items = useSelector((state) => state.items);
-  const finalItems = items.filter((item) => { const itemIndex = bagItems.indexOf(item.id);
-  return itemIndex >= 0;
-  console.log("Final items:", finalItems); // Log finalItems to check its content
-});
+
+  // Filter items to get only those present in the shopping bag
+  const finalItems = items.filter((item) => bagItems.includes(item.id));
+
   return (
     <>
-      <div className="bag-page">
-        <div className="bag-item-container">
-          {finalItems.map((item) =>(
-            <BagItem item={item} />
-          ))}
+      {bagItems.length === 0 ? (
+        <EmptyCart />
+      ) : (
+        <div className="bag-page">
+          <div className="bag-item-container">
+            {finalItems.map((item) => (
+              <BagItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
